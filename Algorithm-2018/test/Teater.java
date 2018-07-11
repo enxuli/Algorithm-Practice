b542462973d547e526624c6357fd372205ea4fc6
Alex Li <enxuli@usc.edu>
2018-07-10 21:14:00 -0400

    New Tester

--- Algorithm-2018/test/Teater.java
+++ Algorithm-2018/test/Teater.java
@@ -1,5 +1,5 @@
 public class Teater {
-	public void Main() {
+	public static void main(String[] args) {
 		Solutions sol = new Solutions();
 		int[] input611 ={1,2,2,3,1,4,2};
 		int sol611 = sol.sol611(input611);
@@ -8,7 +8,7 @@ public class Teater {
 		
 	}
 	
-	public void DisplaySol(String quizindex,String sol) {
+	public static void DisplaySol(String quizindex,String sol) {
 		String output = quizindex + " result:" + sol;
 		System.out.println(output);
 	}
