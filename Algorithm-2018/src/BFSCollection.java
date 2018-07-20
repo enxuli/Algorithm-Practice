f74af7cfb467334105ed6637267c6bad3c70be3c
enxuli <enxuli@usc.edu>
2018-07-20 15:36:57 -0700

    new collection added

---
+++ Algorithm-2018/src/BFSCollection.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.lang.Math;
+import java.util.Random;
+import java.util.Set;
+import java.util.Stack;
+import java.util.Collections;
+
+public class BFSCollection {
+	public class TreeNode {
+		      int val;
+		      TreeNode left;
+		      TreeNode right;
+		      TreeNode(int x) { val = x; }
+		  }
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal){
+        if (root == null) return true;
+        if (root.val <= minVal || root.val >= maxVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+    
+    
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+        
+        while (cur != null || !stack.empty()){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+            
+        return list;
+    }
+    
+    
+    public int kthSmallest(TreeNode root, int k) {
+        int count = countNodes(root.left);
+        if (k <= count) {
+            return kthSmallest(root.left, k);
+        } else if (k > count + 1) {
+            return kthSmallest(root.right, k-1-count); // 1 is counted as current node
+        }
+        
+        return root.val;
+    }
+    
+    public int countNodes(TreeNode n) {
+        if (n == null) return 0;
+        
+        return 1 + countNodes(n.left) + countNodes(n.right);
+    }
+    
+    
+    
+}
--- Algorithm-2018/src/JavaMethods.java
+++ Algorithm-2018/src/JavaMethods.java
@@ -1,3 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.lang.Math;
+import java.util.Random;
+import java.util.Set;
+import java.util.Stack;
+import java.util.Collections;
 
 public class JavaMethods {
 	public perseInt (String s) {
