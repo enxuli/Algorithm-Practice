9206b256b6e51403de30ff81fb564ec1abb393d5
enxuli <enxuli@usc.edu>
2018-07-21 15:37:17 -0700

    new sols added

--- Algorithm-2018/src/Tree.java
+++ Algorithm-2018/src/Tree.java
@@ -108,6 +108,20 @@ public class ListNode {
             return 1 + minDepth(root.right) + minDepth(root.left) ; 
         return 1 + Math.min(minDepth(root.left), minDepth(root.right)); 
     }
+    
+    //101
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+    
+    private boolean isMirror(TreeNode left,TreeNode right){
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        return left.val == right.val && isMirror(left.left,right.right) && isMirror(left.right,right.left);
+    }
+    
+    //
 	
 	
 }
