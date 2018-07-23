5275fb0ce8cdb016c1a6f221d467878bd9ea1b92
enxuli <enxuli@usc.edu>
2018-07-23 01:33:55 -0700

    new sol added

--- Algorithm-2018/src/graph.java
+++ Algorithm-2018/src/graph.java
@@ -77,6 +77,48 @@ public class graph {
         dfs(graphtarget,K);
         return ans;
     }
+    
+    
+    class GraphNode{
+        int val;
+        List<GraphNode> neighbors;
+        GraphNode(int x){
+            val = x;
+            neighbors = new ArrayList<>();
+        }
+    }
+    //834
+
+    boolean[] visited;
+    List<GraphNode> nodeset = new ArrayList<>();
+    int[] ans;
+    public void buildGraph(int[][] edges,int N){
+        for(int k = 0; k < N ; k ++) nodeset.add(new GraphNode(k));
+        for(int i = 0; i < N-1 ; i ++){
+                    nodeset.get(edges[i][0]).neighbors.add(nodeset.get(edges[i][1]));
+                    nodeset.get(edges[i][1]).neighbors.add(nodeset.get(edges[i][0]));
+            }
+        }
+    
+    
+    public void dfs(GraphNode node,int i, int k){
+        if(visited[node.val]){ans[i]+=k-1; return;}
+        else visited[node.val]=true;
+        for(GraphNode neighbor : node.neighbors){
+            dfs(neighbor,i,k+1);
+        }
+        
+    }
+    
+    public int[] sumOfDistancesInTree(int N, int[][] edges) {
+        ans = new int[N];
+        buildGraph(edges,N);
+        for(int i = 0 ; i < N; i ++){
+        visited = new boolean[N];
+        dfs(nodeset.get(i),i,0);
+        }
+        return ans;
+    }
 	
 	
 	
