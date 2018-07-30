0375a6a5df461f4f50db8fb03420282826111796
enxuli <enxuli@usc.edu>
2018-07-29 19:33:55 -0700

    new sol added

--- Algorithm-2018/src/BitManipulation.java
+++ Algorithm-2018/src/BitManipulation.java
@@ -38,6 +38,47 @@ public class BitManipulation {
         public boolean isPowerOfFour(int num) {
               return num > 0 && (num&(num-1)) == 0 && (num & 0x55555555) != 0;
         }
+        
+        //371. Sum of Two Integers toooooo freaking genius
+     // Recursive
+        public int getSum(int a, int b) {
+        	return (b == 0) ? a : getSum(a ^ b, (a & b) << 1);
+        }
+
+        // Recursive
+        public int getSubtract(int a, int b) {
+        	return (b == 0) ? a : getSubtract(a ^ b, (~a & b) << 1);
+        }
+
+        // Get negative number
+        public int negate(int x) {
+        	return ~x + 1;
+        }
+        public int getSum2(int a, int b) {
+        	if (a == 0) return b;
+        	if (b == 0) return a;
+
+        	while (b != 0) {
+        		int carry = a & b;
+        		a = a ^ b;
+        		b = carry << 1;
+        	}
+        	
+        	return a;
+        }
+
+        // Iterative
+        public int getSubtract2(int a, int b) {
+        	while (b != 0) {
+        		int borrow = (~a) & b;
+        		a = a ^ b;
+        		b = borrow << 1;
+        	}
+        	
+        	return a;
+        }
+        
+        
     
 // 477 brute force!! n^2
 	
